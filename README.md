21b78767053a3a4fcb27ffdfffcd819449912e17
Rob Beezer <beezer@ups.edu>
2017-01-13 21:15:12 -0800

    Various infrastructure files

---
+++ README.md
@@ -0,0 +1,20 @@
+Sage and Linear Algebra
+-----------------------
+
+This is a collection of classroom-tested worksheets to accompany Beezer's [First Course in Linear Algebra](http://linear.pugetsound.edu/).  The `worksheets` directory is keyed to the acronyms of seventeen sections of the book (of a possible thirty-seven sections).  There are two purposes:
+  * Understand linear algebra better through computational experiments.
+  * Learn Sage syntax, and Sage commands, for exact linear algebra.
+
+These are designed to be presented in class, and the overview includes time estimates when used that way.  The exercises are designed for an instructor to walk through, and talk through, while presenting the worksheet.  A motivated student, perhaps studying independently, could attempt the exercises.  There are no "solutions," but there is some guidance about commands to employ.
+
+For each section covered there is
+  * PDF: which does not show the empty Sage cells present elsewhere
+  * HTML: a single webpage, with active Sage cells, and empty cells for the exercises.  There is no facility for saving your work with these.
+  * SageMathCloud: the native format on SageMathCloud, `*.sagews`.  Make a (free) account and upload, or pay for an account and just clone this repository with the provided internet access.  Be sure to copy the two CSS files to any folder where you place these files, they will not even render properly as-is.  Some assembly required.
+  * Jupyter Notebooks: experimental.  Functional now, but missing headers, `*.ipynb`.  These are tested in SageMathCloud, elsewhere you may need to locate and select a Sage kernel.  You can similarly save your work with your SageMathCloud account.
+
+There is a bash script, which you can experiment with.  However we are distributing, and updating, all the same outputs via this repository.
+
+See [the overview](worksheets/overview.html) for brief descriptions of each worksheet.  If you plan to present these in class, you might practice the exercises in advance.  They are not hard, but better to be prepared than to stumble around in class.
+
+License:  CC BY-SA ([CreativeCommons Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/))
---
+++ script/build.sh
@@ -0,0 +1,91 @@
+#! /bin/bash
+
+# Sage and Linear Algebra Worksheets #
+#          Robert A. Beezer          #
+#  Copyright 2017 License: CC BY-SA  #
+#  See COPYING for more information  #
+
+# Sage for Linear Algebra worksheets
+#
+# History
+#
+#  2017-01-12  Initiated
+
+##############
+# Instructions
+##############
+
+# 1.  Make this script executable
+# 2.  Check prerequisite binaries below
+
+# Prerequisite binaries/executables
+# $ which <name> will return path if available
+#
+# install
+# xsltproc
+# A LaTeX installation
+#   texfot - utility to restict output
+#   xelatex - preferable for Unicode characters
+# MathBook XML distribution
+
+declare ROOT=${HOME}/books/fcla/sla
+declare SCRATCH=/tmp/sla
+declare MBX=${HOME}/mathbook/mathbook
+declare LATEX="texfot xelatex"
+
+# FCLA sections with worksheets, in order
+ALLSECTIONS=(RREF NM SS MISLE)
+
+# http://stackoverflow.com/questions/12303974/assign-array-to-variable
+# assignment array variable b=( "${a[@]}" )
+if [ "X${1}" == "X" ] ; then
+    echo "Provide the string 'all' or a section acronym"
+    exit
+elif [ "${1}" == "all" ] ; then
+    declare SECTIONS=( "${ALLSECTIONS[@]}" );
+else
+    declare SECTIONS=(${1})
+fi
+
+# Create/use temp/scratch directory
+install -d ${SCRATCH}
+cd ${SCRATCH}
+
+# Overview HTML only
+xsltproc -xinclude ${MBX}/xsl/mathbook-html.xsl  ${ROOT}/worksheets/overview.xml
+xsltproc -xinclude ${MBX}/xsl/mathbook-latex.xsl ${ROOT}/worksheets/overview.xml
+${LATEX} overview.tex
+${LATEX} overview.tex
+
+for SEC in ${SECTIONS[*]}
+do
+  echo "*****************"
+  echo "Processing "${SEC}
+  echo "*****************"
+  ###############################
+  # PDF, process twice with LaTeX
+  ###############################
+  xsltproc -stringparam numbering.theorems.level 0 -xinclude \
+      ${MBX}/xsl/mathbook-latex.xsl ${ROOT}/worksheets/${SEC}/${SEC}.xml
+  ${LATEX} ${SEC}.tex
+  ${LATEX} ${SEC}.tex
+  ######
+  # HTML
+  ######
+  xsltproc -stringparam numbering.theorems.level 0 -stringparam chunk.level 0 \
+      -stringparam html.knowl.exercise.inline no -xinclude \
+      ${MBX}/xsl/mathbook-html.xsl ${ROOT}/worksheets/${SEC}/${SEC}.xml
+  ###############
+  # SageMathCloud
+  ###############
+  xsltproc -stringparam numbering.theorems.level 0 -stringparam chunk.level 0 -xinclude \
+      ${MBX}/xsl/mathbook-smc.xsl ${ROOT}/worksheets/${SEC}/${SEC}.xml
+  #########
+  # Jupyter
+  #########
+  xsltproc -stringparam numbering.theorems.level 0 -stringparam chunk.level 0 -xinclude \
+      ${MBX}/xsl/mathbook-jupyter.xsl ${ROOT}/worksheets/${SEC}/${SEC}.xml
+done
+
+# restore working directory
+cd - > /dev/null
